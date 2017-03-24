107159de1b1f4693fcf82559a8428020569fe0b4
Bastien Kovac <bastien.kovac@gmail.com>
2017-03-24 16:43:23 +0100

    yolo

--- site.php
+++ site.php
@@ -64,7 +64,7 @@ if(isset($_GET['section'])){
     break;
 
     case 'ajoutAdherent':
-        if (isset($_GET['ajouter'])) {
+        if (isset($_POST['ajouter'])) {
             require_once('Controller/inscrireAdherent.php');
         }
     break;
