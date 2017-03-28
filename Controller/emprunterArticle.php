5a4b62f898f283449a2a03e9e7c56b0b5f56eca0
Coustenoble <coustenoble.nathan@gmail.com>
2017-03-28 10:14:43 +0200

    Je vous prie de constater qu'Emprunt est bon.

--- Controller/emprunterArticle.php
+++ Controller/emprunterArticle.php
@@ -1,24 +1,54 @@
 <?php
 //Permet d" emprunter un article si l'on est connecté
 include_once('Model/rechercher.php');
+include_once('Model/emprunter.php');
 $tabUtilisateur = array();
 $tabMedia = array();
-switch ($_GET['action']){
-    case 'rechercheUtilisateur':
-        $tabUtilisateur = get_AdherentByParam($_POST['username']);
-        break;
-    case 'selectionUtilisateur':
-        break;
-    case 'rechercheMedia':
-        $tabMedia = get_MediaByParam($_POST['username']);
-        break;
-    case 'selectionMedia':
-        break;
-    default:
 
-        break;
+if(isset($_GET['user'])){
+    $user = $_GET['user'];
+}else{
+    $user ="";
 }
-include('View/mediaEmprunte.php');
+
+if(isset($_GET['media'])){
+    $media = $_GET['media'];
+    $type = $_GET['type'];
+}else{
+    $media ="";
+    $type ="";
+}
+if(isset($_GET['action'])){
+    switch ($_GET['action']){
+        case 'rechercheUtilisateur':
+            $etape = 2;
+            $tabUtilisateur = get_AdherentByParam($_POST['username']);
+            include('View/mediaEmprunte.php');
+            break;
+        case 'selectionUtilisateur':
+            $etape = 3;
+            include('View/mediaEmprunte.php');
+            break;
+        case 'rechercheMedia':
+            $etape = 4;
+            $user = $_POST['user'];
+            $tabMedia = get_MediaByParam($_POST['media']);
+            include('View/mediaEmprunte.php');
+            break;
+        case 'selectionMedia':
+            emprunter($_GET['user'],$_GET['type'],$_GET['media']);
+            include('index.php');
+            break;
+        default:
+
+            break;
+    }
+}else{
+    $etape = 1;
+    include('View/mediaEmprunte.php');
+}
+
+
     /*include('Model/ConnexionBD.php');
     include('Model/OperationsMedia.php');
     $media = get_media(htmlentities($_GET['idMedia']),htmlentities($_GET['typeMedia']));
--- Model/rechercher.php
+++ Model/rechercher.php
@@ -3,9 +3,11 @@
 //Retourne les emprunts de type livre d'un adhérent
 function get_AdherentByParam($param){
     include('Model/ConnexionBD.php');
-    $query=$bdd->prepare("SELECT * FROM adherent
-          WHERE nom LIKE '%:param%'
-          OR prenom LIKE '%:param%';");
+    $param = '%'.$param.'%';
+    $var = "SELECT * FROM adherent
+          WHERE nom LIKE :param
+          OR prenom LIKE :param;";
+    $query=$bdd->prepare($var);
     $query->bindParam(':param', $param, PDO::PARAM_STR);
     $query->execute();
     $adherents = $query->fetchAll();
@@ -14,23 +16,24 @@ function get_AdherentByParam($param){
 
 function get_MediaByParam($param){
     include('Model/ConnexionBD.php');
+    $param = '%'.$param.'%';
     //dvd
-    $query=$bdd->prepare("SELECT * FROM dvd
-          WHERE titre LIKE '%:param%';");
+    $query=$bdd->prepare("SELECT id,titre,'dvd' as categorie, genre FROM dvd
+          WHERE titre LIKE :param;");
     $query->bindParam(':param', $param, PDO::PARAM_STR);
     $query->execute();
     $dvds = $query->fetchAll();
 
     //livre
-    $query=$bdd->prepare("SELECT * FROM livre
-          WHERE titre LIKE '%:param%';");
+    $query=$bdd->prepare("SELECT id,titre,'livre' as categorie, type as genre FROM livre
+          WHERE titre LIKE :param;");
     $query->bindParam(':param', $param, PDO::PARAM_STR);
     $query->execute();
     $livres = $query->fetchAll();
 
     //cd
-    $query=$bdd->prepare("SELECT * FROM cd
-          WHERE titre LIKE '%:param%';");
+    $query=$bdd->prepare("SELECT id,titre,'cd' as categorie, genre FROM cd
+          WHERE titre LIKE :param;");
     $query->bindParam(':param', $param, PDO::PARAM_STR);
     $query->execute();
     $cds = $query->fetchAll();
